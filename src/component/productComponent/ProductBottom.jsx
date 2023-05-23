cf29dec85c50a2e35e9a3bd9343ca675acb1b63a
NguyenVanLinh12345 <truongtansang61@gmail.com>
2023-05-23 14:02:19 +0700

    23-05-2023

--- src/component/productComponent/ProductBottom.jsx
+++ src/component/productComponent/ProductBottom.jsx
@@ -3,15 +3,36 @@ import Row from '../Grid/Row';
 import Col from '../Grid/Col';
 
 import style from './ProductBottom.module.scss';
+import { FaStar } from 'react-icons/fa';
+
 import { useEffect, useState } from 'react';
 
 function ProductBottom({ moTa, idSanPham }) {
 
-    const [danhSachhBinhLuan, setDSBL] = useState([]);
+    const [danhSachhBinhLuan, setDSBL] = useState([
+        {
+            id: 1,
+            tenNguoiBinhLuan: "Nguyen Van A",
+            ngayBinhLuan: "2023-12-12",
+            noiDung: "Hang cuc ki tot nha",
+            soSao: 4
+        }
+    ]);
+    // const [danhSachhBinhLuan, setDSBL] = useState([]);
 
+    const [luaChon, setLuaChon] = useState(0);
+
+    const [star, setStar] = useState(5);
     const [ten, setTen] = useState("");
     const [email, setEmail] = useState("");
     const [noiDung, setNoiDung] = useState("");
+
+    const toListStar = (length) => {
+        let mangTrong = [];
+        for (let i = 1; i <= length; i++)
+            mangTrong.push(<FaStar key={i} />);
+        return mangTrong;
+    }
 
     const postBinhLuan = () => {
 
@@ -28,7 +49,7 @@ function ProductBottom({ moTa, idSanPham }) {
             })
         })
             .then(response => response.json())
-            .then(data =>{
+            .then(data => {
                 setDSBL([...danhSachhBinhLuan, data])
                 setEmail("");
                 setNoiDung("");
@@ -45,14 +66,12 @@ function ProductBottom({ moTa, idSanPham }) {
             .catch(error => console.error(error))
     }, [idSanPham])
 
-
-    const [luaChon, setLuaChon] = useState(0);
     return (
         <Grid myClass='mg_bt40'>
             <Row>
                 <Col pc={12} table={12} mobie={12}>
-                    <button style={{ borderBottom: luaChon === 0 ? "0.2rem solid white" : "none", backgroundColor: luaChon === 0 ? "lightgray" : "white" }} className={style.the_tren} onClick={() => setLuaChon(0)}>Mo ta</button>
-                    <button style={{ borderBottom: luaChon === 1 ? "0.2rem solid white" : "none", backgroundColor: luaChon === 1 ? "lightgray" : "white" }} className={style.the_tren} onClick={() => setLuaChon(1)}>Danh Gia</button>
+                    <button className={`${style.the_tren} ${luaChon === 0 ? style.focus_lua_chon : ""}`} onClick={() => setLuaChon(0)}>Mo ta</button>
+                    <button className={`${style.the_tren} ${luaChon === 1 ? style.focus_lua_chon : ""}`} onClick={() => setLuaChon(1)}>Danh Gia</button>
                     <div className={style.conntainer}>
 
                         {
@@ -64,30 +83,50 @@ function ProductBottom({ moTa, idSanPham }) {
                                     {
                                         danhSachhBinhLuan.map((value) => (
                                             <div key={value.id} className={style.binh_luan}>
-                                                <h1>{value.tenNguoiBinhLuan}</h1>
+                                                <h1>{value.tenNguoiBinhLuan}<span>{toListStar(value.soSao)}</span></h1>
                                                 <h3>{value.ngayBinhLuan}</h3>
                                                 <p>{value.noiDung}</p>
                                             </div>
                                         ))
                                     }
-                                    <div className={style.them_binh_luan} >
-                                        <div>
-                                            <label htmlFor="">Ten cua ban:</label>
-                                            <input placeholder='Nhap ten...' onChange={(e) => setTen(e.target.value)} type="text" value={ten} />
-                                        </div>
-                                        <div>
-                                            <label htmlFor="">Email cua ban:</label>
-                                            <input placeholder='Nhap email...' onChange={(e) => setEmail(e.target.value)} type="email" value={email} />
-                                        </div>
+                                    <Grid myClass={style.them_binh_luan} checkWide={false}>
+                                        <Row>
+                                            <Col pc={12} table={12} mobie={12}>
+                                                <label htmlFor="">Danh gia cua ban:</label>
+                                                <div className={style.list_star}>
+                                                    <span className={star === 1 ? style.choose_star : ""} onClick={() => setStar(1)}><FaStar /></span>
+                                                    <span className={star === 2 ? style.choose_star : ""} onClick={() => setStar(2)}><FaStar /><FaStar /></span>
+                                                    <span className={star === 3 ? style.choose_star : ""} onClick={() => setStar(3)}><FaStar /><FaStar /><FaStar /></span>
+                                                    <span className={star === 4 ? style.choose_star : ""} onClick={() => setStar(4)}><FaStar /><FaStar /><FaStar /><FaStar /></span>
+                                                    <span className={star === 5 ? style.choose_star : ""} onClick={() => setStar(5)}><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></span>
+                                                </div>
+                                            </Col>
+                                        </Row>
 
-                                        <div>
-                                            <label htmlFor="">Noi Dung:</label>
-                                            <input className={style.cho_noi_dung} placeholder='Nhap noi dung...' onChange={(e) => setNoiDung(e.target.value)} type="email" value={noiDung} />
-                                        </div>
+                                        <Row myClass='mg_t20'>
+                                            <Col pc={12} table={12} mobie={12}>
+                                                <label htmlFor="">Nhan xet cua ban:</label>
+                                                <textarea className={style.cho_noi_dung} placeholder='Nhap noi dung...' onChange={(e) => setNoiDung(e.target.value)} type="email" value={noiDung} />
+                                            </Col>
+                                        </Row>
 
+                                        <Row myClass='mg_t20'>
+                                            <Col pc={6} table={6} mobie={12}>
+                                                <label htmlFor="">Ten cua ban:</label>
+                                                <input placeholder='Nhap ten...' onChange={(e) => setTen(e.target.value)} type="text" value={ten} />
+                                            </Col>
 
-                                        <button onClick={postBinhLuan}>Them Binh Luan</button>
-                                    </div>
+                                            <Col pc={6} table={6} mobie={12}>
+                                                <label htmlFor="">Email cua ban:</label>
+                                                <input placeholder='Nhap email...' onChange={(e) => setEmail(e.target.value)} type="email" value={email} />
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col myClass='mg_t20 mg_bt40' pc={12} table={12} mobie={12}>
+                                                <button className={style.gui_di} onClick={postBinhLuan}>Them Binh Luan</button>
+                                            </Col>
+                                        </Row>
+                                    </Grid>
                                 </div>
                         }
                     </div>
